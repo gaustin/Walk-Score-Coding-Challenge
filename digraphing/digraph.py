bd1e9641bee79255fac4fe035757c066d003eb12
Grant Austin <gaustin@mt.gov>
2012-02-27 14:40:43 -0700

    Call edges arcs and be more regular in nodes/arcs usage. Added method to prune nodes with 1 in-arc and one out-arc.

--- digraphing/digraph.py
+++ digraphing/digraph.py
@@ -2,7 +2,7 @@ class Digraph:
     def __init__(self):
         self.graph = dict()
 
-    def addEdge(self, fromNode, toNode):
+    def addArc(self, fromNode, toNode):
         if not self.graph.has_key(fromNode):
             self.graph[fromNode] = set() 
         if not self.graph.has_key(toNode):
@@ -10,12 +10,39 @@ class Digraph:
         
         self.graph[fromNode].add(toNode)
 
-    def removeEdge(self, fromNode, toNode):
+    def removeArc(self, fromNode, toNode):
         if not self.graph.has_key(fromNode) or not self.graph.has_key(toNode):
             raise NodeError("A given node does not exist in the graph.")
         
-        # Delete the node
+        # Delete the arc
         self.graph[fromNode].discard(toNode)
+
+    def addOrUpdateNode(self, node, outArcs=None):
+        if outArcs is None:
+            outArcs = set()
+
+        self.graph[node] = outArcs
+
+    def compress(self):
+        # Non-destructive 
+        compressedGraph = Digraph()
+        for node, outArcs in self.graph.items():
+            in_arcs = self.inArcs(node)
+            if len(outArcs) == 1 and len(in_arcs) == 1:
+                compressedGraph.addArc(in_arcs.pop(), outArcs.copy().pop()) 
+            else:
+                compressedGraph.addOrUpdateNode(node, outArcs)
+
+        return compressedGraph
+
+    def inArcs(self, targetNode):
+        # Return all of the arcs pointing at this node
+        in_arcs = set()
+        for node, outArcs in self.graph.items():
+            if targetNode in outArcs:
+                in_arcs.add(node)
+
+        return in_arcs
 
     def has_arc(self, fromNode, toNode):
         return toNode in self.graph[fromNode]
@@ -25,6 +52,9 @@ class Digraph:
             return True
         else:
             return False
+
+    def nodeCount(self):
+        return len(self.graph.keys())
 
 class NodeError(Exception):
     def __init__(self, message):
--- test/test_digraph.py
+++ test/test_digraph.py
@@ -7,38 +7,78 @@ import unittest
 
 from digraphing import Digraph, NodeError
 
+def better_graph():
+    # A B
+    # B C
+    # C D
+    # B D
+    # D E
+    # E F
+    # E G
+    # G H
+    # G A
+    graph = Digraph()
+    graph.addArc("A", "B")
+    graph.addArc("B", "C")
+    graph.addArc("C", "D")
+    graph.addArc("B", "D")
+    graph.addArc("D", "E")
+    graph.addArc("E", "F")
+    graph.addArc("E", "G")
+    graph.addArc("G", "H")
+    graph.addArc("G", "A")
+    return graph
+
 def dummy_graph():
     # A B
     # B C
     # A D 
     # D C
     graph = Digraph()
-    graph.addEdge("A", "B")
-    graph.addEdge("B", "C")
-    graph.addEdge("A", "D")
-    graph.addEdge("D", "C")
+    graph.addArc("A", "B")
+    graph.addArc("B", "C")
+    graph.addArc("A", "D")
+    graph.addArc("D", "C")
     return graph
 
 class DigraphTests(unittest.TestCase):
     def setUp(self):
         self.graph = dummy_graph()
 
-    def testAddAnEdge(self):
+    def testAddAnArc(self):
         a = "A"
         b = "Z"
-        self.graph.addEdge(a, b)
+        self.graph.addArc(a, b)
         self.assertTrue(self.graph.has_arc(a, b))
 
-    def testRemoveAnEdge(self):
-        self.graph.removeEdge("A", "D")
+    def testRemoveAnArc(self):
+        self.graph.removeArc("A", "D")
 
         self.assertTrue(self.graph.has_arc("D", "C"))
         self.assertTrue(self.graph.has_arc("A", "B"))
         self.assertTrue(self.graph.has_arc("B", "C"))
         self.assertFalse(self.graph.has_arc("A", "D"))
     
-    def testRemoveNonexistentEdge(self):
-        self.assertRaises(NodeError, self.graph.removeEdge, "A", "Z")
+    def testRemoveNonexistentArc(self):
+        self.assertRaises(NodeError, self.graph.removeArc, "A", "Z")
+
+    def testCompressDummyGraph(self):
+        compressedGraph = self.graph.compress()
+
+        self.assertTrue(compressedGraph.has_arc("A", "C"))
+        self.assertEqual(2, compressedGraph.nodeCount())
+
+    def testCompressBetterGraph(self):
+        compressedGraph = better_graph().compress()
+        
+        self.assertTrue(compressedGraph.has_arc("B", "D"))
+        self.assertTrue(compressedGraph.has_arc("D", "E"))
+        self.assertTrue(compressedGraph.has_arc("E", "F"))
+        self.assertTrue(compressedGraph.has_arc("E", "G"))
+        self.assertTrue(compressedGraph.has_arc("G", "H"))
+        self.assertTrue(compressedGraph.has_arc("G", "A"))
+
+        self.assertEqual(6, compressedGraph.nodeCount())
 
 if __name__ == "__main__":
     unittest.main()
